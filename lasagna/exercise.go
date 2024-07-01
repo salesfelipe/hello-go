12dca90442b061dcbdb021a1106db98c434ca69f
Felipe Sales <sales.felipeb@gmail.com>
2024-07-01 08:43:17 +0200

    Implement lasagna exercise

--- lasagna/exercise.go
+++ lasagna/exercise.go
@@ -1,34 +1,47 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+const NOODLES_G = 50
+const SAUCE_L = 0.2
 
 func PreparationTime(layers []string, avgPrep int) int {
+	if avgPrep == 0 {
+		return len(layers) * 2
+	}
+
 	return len(layers) * avgPrep
 }
 
-func Quantities() {
+func Quantities(layers []string) (int, float64) {
+	noodlesCount := 0
+	sauceCount := 0
 
+	for i := 0; i < len(layers); i++ {
+		layer := layers[i]
+
+		if layer == "noodles" {
+			noodlesCount++
+		} else if layer == "sauce" {
+			sauceCount++
+		}
+	}
+
+	return noodlesCount * NOODLES_G, float64(sauceCount) * SAUCE_L
 }
 
-func AddSecretIngredient() {
+func AddSecretIngredient(friendsList, myList []string) {
+	secretIngredient := friendsList[len(friendsList)-1]
 
+	myList[len(myList)-1] = secretIngredient
 }
 
-func ScaleRecipe() {
+func ScaleRecipe(quantities []float64, portions int) []float64 {
+	scaled := make([]float64, len(quantities))
 
+	multiplier := float64(portions) / 2
+
+	for i := 0; i < len(quantities); i++ {
+		scaled[i] = quantities[i] * multiplier
+	}
+
+	return scaled
 }
-
-// TODO: define the 'Quantities()' function
-
-// TODO: define the 'AddSecretIngredient()' function
-
-// TODO: define the 'ScaleRecipe()' function
-
-// Your first steps could be to read through the tasks, and create
-// these functions with their correct parameter lists and return types.
-// The function body only needs to contain `panic("")`.
-//
-// This will make the tests compile, but they will fail.
-// You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more
-// functionality.
