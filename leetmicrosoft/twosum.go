038f507f906a021ca1153b3bd5212dc5e51328be
Felipe Sales <sales.felipeb@gmail.com>
2024-07-30 08:18:36 +0200

    Implement twosum with hashmap

--- leetmicrosoft/twosum.go
+++ leetmicrosoft/twosum.go
@@ -1,15 +1,36 @@
 package leetmicrosoft
 
 func twoSum(nums []int, target int) []int {
+	values := make(map[int][]int)
+
+	for i := 0; i < len(nums); i++ {
+		indexes, ok := values[nums[i]]
+
+		if ok {
+			values[nums[i]] = append(indexes, i)
+		} else {
+			values[nums[i]] = []int{i}
+		}
+
+	}
 
 	result := make([]int, 0)
 	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				result = append(result, i, j)
+		rest := target - nums[i]
 
-				break
+		indexes, ok := values[rest]
+
+		if ok {
+			for _, j := range indexes {
+				if j != i {
+					result = append(result, i, j)
+					break
+				}
 			}
+		}
+
+		if len(result) != 0 {
+			break
 		}
 	}
 
