b0252dce8b7a9c025f55f9e4bd8520fb6c3ab37f
Felipe Sales <sales.felipeb@gmail.com>
2024-07-30 07:55:16 +0200

    Implement bruteforse approach

--- leetmicrosoft/twosum.go
+++ leetmicrosoft/twosum.go
@@ -2,5 +2,16 @@ package leetmicrosoft
 
 func twoSum(nums []int, target int) []int {
 
-	return nums
+	result := make([]int, 0)
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				result = append(result, i, j)
+
+				break
+			}
+		}
+	}
+
+	return result
 }
--- leetmicrosoft/twosum_test.go
+++ leetmicrosoft/twosum_test.go
@@ -12,6 +12,8 @@ type CaseTw struct {
 
 var casesTw []CaseTw = []CaseTw{
 	{inputNums: []int{2, 7, 11, 15}, inputTarget: 9, expect: []int{0, 1}},
+	{inputNums: []int{3, 2, 4}, inputTarget: 6, expect: []int{1, 2}},
+	{inputNums: []int{3, 3}, inputTarget: 6, expect: []int{0, 1}},
 }
 
 func TestTwoSum(t *testing.T) {
